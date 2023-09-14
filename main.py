dbd43fd8e1ceb16d7b3a515ccd1a929971606cf5
dbdzx <andrew.andrsen.andrew@gmail.com>
2023-09-14 15:48:10 +0400

    change main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+app.config['SECRET'] == "secret!123"
+socketio = SocketIO(app, cors_allowed_origins = "*")
  
 @app.route('/')
 def hello_world():
- return 'Moe Flask приложение в контейнере Docker!'
+ return 'Moe Flask приложение в контейнере Docker на сервере gunicorn!'
  
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
+    
